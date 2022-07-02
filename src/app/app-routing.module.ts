b9c7ba38e3d26b73a6ed8089234d6bd81e4128d3
Venkatesh Prasad V <venkateshprasad0@gmail.com>
2022-07-03 01:16:05 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DownloadsComponent } from './downloads/downloads.component';
+import { FriendComponent } from './friend/friend.component';
+import { FriendsComponent } from './friends/friends.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'downloads', component: DownloadsComponent},
+  {path: 'friends', component: FriendsComponent},
+  {path: 'friends/:id', component:FriendComponent },
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { DownloadsComponent } from './downloads/downloads.component';
+import { FriendComponent } from './friend/friend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FriendsComponent,
+    DownloadsComponent,
+    FriendComponent
   ],
   imports: [
     BrowserModule,
